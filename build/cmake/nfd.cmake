ed2cebc8457f0152efbe8f644815999b5c53c2e2
Hjalmar <github.disk321@passinbox.com>
2024-08-10 15:27:28 +0200

    changed output dir for build

--- build/cmake/nfd.cmake
+++ build/cmake/nfd.cmake
@@ -4,8 +4,8 @@ cmake_minimum_required(VERSION 3.10)
 project(nfd)
 
 # Set output directories for the build types
-set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG ${CMAKE_BINARY_DIR}/../lib/Debug/x64)
-set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE ${CMAKE_BINARY_DIR}/../lib/Release/x64)
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG ../../lib/Debug/x64)
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE ../../lib/Release/x64)
 
 # Set source files
 set(SOURCE_FILES
